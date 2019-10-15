8185938d6d41f081d79b8deecd7253617c9652bb
Seungjin Lee <lsg0385@naver.com>
2019-10-15 15:45:11 +0900

    making router

--- klaytn_communication_api/shoppingmall/api_server.js
+++ klaytn_communication_api/shoppingmall/api_server.js
@@ -1,123 +1,18 @@
-var manage = require('./manage.js.js');
-
-var abi = require('./build/contracts/ShoppingMallItem.json');
-var bytecode = abi.bytecode;
-abi = abi.abi;
-var cav = manage.cav;
-var item = new cav.klay.Contract(abi);
-
-var api = {
-    registerItem: async function(distribution, totalPrice, ownerAddress) {
-        if(!this.isValidItem(distribution, totalPrice, ownerAddress)) {
-            console.log(distribution);
-            console.log(totalPrice);
-            console.log(ownerAddress)
-            console.log('Invalid type or number of inputs');
-            return;
-        }
-        manage.server.Login();
-
-        await item.deploy({data: bytecode, arguments: [distribution, totalPrice, ownerAddress]})
-        .send({from: cav.klay.accounts.wallet[0].address, value: 0, gas: 9990000, gasLimit: 9990000}, (err, txhash) => {
-            if (err) {
-                console.log(err);
-                throw err;
-            }
-        })
-        .on('error', (err) => {
-            console.log(err);
-            manage.server.clearWallet();
-            return;
-        })
-        .on('receipt', (res) => {
-            itemAddress = res.contractAddress;
-        })
-        
-        manage.server.clearWallet();
-        return itemAddress;
-    },
-    isValidItem: function(distribution, totalPrice, ownerAddress) {
-        if(ownerAddress[0] != '0' || ownerAddress[1] != 'x') {
-            console.log('Address should be started by 0x');
-            return;
-        }
-        return ownerAddress.length == 42 && distribution && totalPrice;
-    },
-    staking: async function(contractAddress, buyerAddress) {
-        if (!contractAddress && buyerAddress) {
-            console.log("You missed 1 or 2 arguments!");
-            return;
-        }
-        end = true;
-        manage.server.Login();
-        item.options.address = contractAddress;
-        await item.methods.Staking(buyerAddress)
-        .send({from: cav.klay.accounts.wallet[0].addㄴress, gas: 8990000, gasLimit: 8990000, value: 1})
-        .then('receipt', (receipt) => {
-            if (JSON.parse(receipt)['status'] == false) end = false;
-        })
-        .then('error', (err) => {
-            // console.log(err);
-            end = false;
-            return;
-        })
-        manage.server.clearWallet();
-        return end;
-    },
-    getWinner: async function(contractAddress) {
-        if (!contractAddress) {
-            console.log("You missed contractaddress");
-            return;
-        }
-        let winner;
-        manage.server.Login();
-        item.options.address = contractAddress;
-        console.log(cav.klay.accounts.wallet[0])
-        await item.methods.lottery()
-        .send({from: cav.klay.accounts.wallet[0].address, gas: 900000, gasLimit: 900000, value: 0})
-        .then('receipt', (receipt)=> {
-            console.log(receipt);
-            if (JSON.parse(receipt)['status'] == false) end = false;
-        })
-        .then('error', (err) => {
-            console.log(err);
-            end =false;
-            return;
-        })
-        .then(async () => {
-            await item.methods.getWinner()
-            .call({from: cav.klay.accounts.wallet[0].address})
-            .then((res)=> {
-                winner = res;
-            })
-        })
-        .catch((err)=> {
-            console.log(err);
-        })
-        
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const router = require("./route/router");
+const bodyParser = require('body-parser');
 
 
-        manage.server.clearWallet();
-        return winner;
-    },
-    remainTicket: async function(contractAddress) {
-        if (!contractAddress) {
-            console.log("You missed contractaddress");
-            return;
-        }
-        manage.server.Login();
-        item.options.address = contractAddress;
-        let number;
-    
-        await item.methods.remainStaking()
-        .call({from: cav.klay.accounts.wallet[0].address})
-        .then((res)=> {
-            number = res;
-        })
-        .then()
-        manage.server.clearWallet();
-        return number;
-    }
-}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+//false일 경우 String Object로만 받고
+//true일 경우엔 어떤 타입이든지 다 받을 수 있다.
 
-module.exports = api;
+app.use(router);
+
+app.listen(port, err => {
+    if(err) console.log(err);
+    else console.log("서버가 가동되었습니다.")
+});
---
+++ klaytn_communication_api/shoppingmall/route/router.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const route = express.Router();
+const api = require('../api');
+
+route.route('/register')
+.post((req, res) => {
+    input = 
+    api.registerItem()
+});
+
+
+
+module.exports = route
